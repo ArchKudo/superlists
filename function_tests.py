9c809b62090bc940d77901fa9afe036c92e77711
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-24 15:52:57 +0530

    Modularize tests a bit to include two items in to-do list table

--- function_tests.py
+++ function_tests.py
@@ -12,6 +12,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, table_id, row_text):
+        table = self.browser.find_element_by_id(table_id)
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def enter_item(self, inputbox, input_text):
+        inputbox.send_keys(input_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
     def test_can_start_list(self):
         # A user story
 
@@ -31,17 +41,12 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # Can add a new to-do item using ENTER key
-        inputbox.send_keys('TO-DO ITEM 1')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.enter_item(inputbox, 'TO-DO ITEM 1')
+        self.enter_item(inputbox, 'TO-DO ITEM 2')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: TO-DO ITEM 1' for row in rows),
-            'New to-do item does not appear in table!\n'
-            f'Contents were: {table.text}\n'
-        )
+        # New to-do item is correctly displayed
+        self.check_for_row_in_list_table('id_list_table', '1: TO-DO ITEM 1')
+        self.check_for_row_in_list_table('2: TO-DO ITEM 2')
 
         self.fail('Finished Test!')  # Delibrately fail
 
