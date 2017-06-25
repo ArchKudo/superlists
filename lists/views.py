6db18e053252184cc8c7fe8a2028527a42aa1fc6
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-25 20:03:41 +0530

    Use Item() instead of single Http POST

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from lists.models import Item
 # from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home_page(request):
+
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-                  'new_item_text': request.POST.get('item_text', '')
+                  'new_item_text': item.text
                   })  # render creates HttpResponse for us
