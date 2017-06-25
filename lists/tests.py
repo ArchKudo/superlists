da28c1772ff8904b75df1198a70a36a14a3f12df
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-25 20:02:07 +0530

    Fixed typo, Test save of post requests

--- lists/tests.py
+++ lists/tests.py
@@ -10,8 +10,13 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  # Client is used for get/post requests
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
+    def test_home_page_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
@@ -30,5 +35,5 @@ class ItemModelTest(TestCase):
         saved_item = Item.objects.all()
         self.assertEqual(saved_item.count(), 2)
 
-        self.assertEqual(saved_item[0], 'The first item')
-        self.assertEqual(saved_item[1], 'The second item')
+        self.assertEqual(saved_item[0].text, 'The first item')
+        self.assertEqual(saved_item[1].text, 'The second item')
