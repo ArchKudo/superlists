92318571b76b9091a170bd98d085f2cd1a467ec3
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-26 16:07:11 +0530

    Hand revert to older functions which caused regressions in selenium

--- function_tests.py
+++ function_tests.py
@@ -8,21 +8,17 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        # self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, table_id, row_text):
-        table = self.browser.find_element_by_id(table_id)
-        rows = table.find_element_by_tag_name('tr')
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def enter_item(self, inputbox, input_text):
-        inputbox.send_keys(input_text)
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-    def test_can_start_list(self):
+    def test_can_start_list_and_retrieve_later(self):
         # A user story
 
         # Can checkout homepage
@@ -41,12 +37,20 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # Can add a new to-do item using ENTER key
-        self.enter_item(inputbox, 'TO-DO ITEM 1')
-        self.enter_item(inputbox, 'TO-DO ITEM 2')
+        inputbox.send_keys('TO-DO ITEM 1')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: TO-DO ITEM 1')
 
         # New to-do item is correctly displayed
-        self.check_for_row_in_list_table('id_list_table', '1: TO-DO ITEM 1')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('TO-DO ITEM 2')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         self.check_for_row_in_list_table('2: TO-DO ITEM 2')
+        self.check_for_row_in_list_table('1: TO-DO ITEM 1')
 
         self.fail('Finished Test!')  # Delibrately fail
 
