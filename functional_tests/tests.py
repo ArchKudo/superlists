b0382e8ed4115765057a2aa3ca3ff5e34e87ddbe
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-26 21:24:45 +0530

    Removed time.sleeps()
    Added wait to function to check for entry in list table

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
@@ -13,10 +14,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        MAX_WAIT = 10
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_list_and_retrieve_later(self):
         # A user story
@@ -41,7 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('1: TO-DO ITEM 1')
+        self.wait_for_row_in_list_table('1: TO-DO ITEM 1')
 
         # New to-do item is correctly displayed
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -49,7 +59,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('2: TO-DO ITEM 2')
-        self.check_for_row_in_list_table('1: TO-DO ITEM 1')
+        self.wait_for_row_in_list_table('2: TO-DO ITEM 2')
+        self.wait_for_row_in_list_table('1: TO-DO ITEM 1')
 
         self.fail('Finished Test!')  # Delibrately fail
