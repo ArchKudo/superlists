efdcb92ebe6601b8b1912d55b396d7119d159428
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-28 16:25:06 +0530

    Initial styling tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -100,3 +100,15 @@ class NewVisitorTest(LiveServerTestCase):
         body_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('TO-DO ITEM FROM FIRST USER 1', body_text)
         self.assertIn('TO-DO ITEM FROM SECOND USER 1', body_text)
+
+    def test_layout_styling(self):
+        self.browser.get(self.live_server_url)
+
+        # Window is 1024 x 768
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        # Inputbox is at centre of the screen
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
