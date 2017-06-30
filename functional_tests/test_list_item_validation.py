d3e18f6f652b99fb69525b0758718a9a30ef0843
Girish Vyas <vgd.vyas17@gmail.com>
2017-06-30 06:54:23 +0530

    Added missing code

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,6 +7,9 @@ class ItemValidationTest(FunctionalTestSetup):
     def test_cannot_add_empty_list_items(self):
 
         # Try Submititting blank items by pressing ENTER
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
